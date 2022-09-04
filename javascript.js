c83b3b2e7e3114940d3d133507a6838614925157
David Gallardo <davidg412@outlook.com>
2022-09-04 14:07:05 -0700

    Initiate for loop in js to create grid for divs in container
    Set css style to grid

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//Loop to create 16x16 grid of divs (256 squares total)
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    container.appendChild(div);
+}
+
+//Create event listener for the hover effect
+function startSketch() {
+    //Create event listener for mouse hovering
+    document.querySelectorAll('div');
+    div.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+        onmouseover = (e)
+        })
+    })
+}
