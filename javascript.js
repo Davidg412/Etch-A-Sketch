ee8bad3e66a89fe72171215960fc9a88532ee712
David Gallardo <davidg412@outlook.com>
2022-09-05 13:15:07 -0700

    Create mouseover effect so that cells turn black when the mouse hovers over them

--- javascript.js
+++ javascript.js
@@ -1,17 +1,20 @@
+function sketch() {
 //Loop to create 16x16 grid of divs (256 squares total)
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    //adds class name of "cell" to div
     div.classList.add('cell');
+    //adds div in html to become a child of container
     container.appendChild(div);
 }
-
 //Create event listener for the hover effect
-function startSketch() {
-    //Create event listener for mouse hovering
-    document.querySelectorAll('div');
+    //Selects all of the classes that are called "cell" and store in variable div
+    const div = document.querySelectorAll('.cell');
+    //forEach goes through all of the variable's contents
     div.forEach((div) => {
         div.addEventListener('mouseover', () => {
-        onmouseover = (e)
+        div.style.backgroundColor = 'black';
         })
     })
 }
+sketch();
