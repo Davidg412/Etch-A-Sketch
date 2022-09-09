df50f78dea086ac7b2d2672653b95cb54465df45
David Gallardo <davidg412@outlook.com>
2022-09-08 21:27:36 -0700

    Create button that prompts user to change current grid size

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 function sketch() {
 //Loop to create 16x16 grid of divs (256 squares total)
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    //adds class name of "cell" to div
-    div.classList.add('cell');
-    //adds div in html to become a child of container
-    container.appendChild(div);
-}
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        //adds class name of "cell" to div
+        div.classList.add('cell');
+        //adds div in html to become a child of container
+        container.appendChild(div);
+    }
+
 
 //Create event listener for the hover effect
     //Selects all of the classes that are called "cell" and store in variable div
@@ -18,4 +19,20 @@ for (let i = 0; i < 256; i++) {
         })
     })
 }
+
+
+//Create event listener to prompt user to input integer if they press the "change size" button
+function changeSize (size) {
+    const changeSize = document.querySelector('.button1');
+        changeSize.addEventListener('click', () => {
+        let size = prompt('Enter an integer for your desired grid-size (max = 100):');
+        // Checks if the user's input is an integer
+            if (Number.isInteger(size) = true) {
+                
+
+            }
+        })
+    }
+changeSize();
+
 sketch();
