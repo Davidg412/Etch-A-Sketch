6f9368b943609b89e64da582aba8bf52e0d3cf71
David Gallardo <davidg412@outlook.com>
2022-09-12 12:27:12 -0700

    Move CSS grid size to javascript so that we are able to edit the grid size

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
-function sketch() {
+//Create size of the grid
+function sketch(size) {
+let sketch = document.querySelector('#container');
+sketch.style.gridTemplateColumns = "repeat(16 , 1fr)";
+sketch.style.gridTemplateRows = "repeat(16 , 1fr)";
+
+
 //Loop to create 16x16 grid of divs (256 squares total)
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         //adds class name of "cell" to div
         div.classList.add('cell');
         //adds div in html to become a child of container
-        container.appendChild(div);
+        sketch.insertAdjacentElement('beforeend', div);
     }
-
+}
+sketch(16);
 
 //Create event listener for the hover effect
     //Selects all of the classes that are called "cell" and store in variable div
@@ -18,7 +25,6 @@ function sketch() {
         div.style.backgroundColor = 'black';
         })
     })
-}
 
 
 //Create event listener to prompt user to input integer if they press the "change size" button
@@ -34,6 +40,7 @@ function changeSize () {
                     const div = document.createElement('div');
                     //adds class name of "cell" to div
                     div.classList.add('cell');
+                    const newContainer = document.querySelector('#container');
                     //adds div in html to become a child of container
                     container.appendChild(div);
                 }
