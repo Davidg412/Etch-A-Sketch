92b0783f8c33c8c46a2ca87e0c6c7b26cecb4783
David Gallardo <davidg412@outlook.com>
2022-09-12 22:03:25 -0700

    Add change size button so that the user can change the etch-a-sketch-size

--- javascript.js
+++ javascript.js
@@ -1,54 +1,30 @@
 //Create size of the grid
 function sketch(size) {
-let sketch = document.querySelector('#container');
-sketch.style.gridTemplateColumns = "repeat(16 , 1fr)";
-sketch.style.gridTemplateRows = "repeat(16 , 1fr)";
+    let sketch = document.querySelector('#container');
+    let cells = sketch.querySelectorAll('div');
+    cells.forEach((div) => div.remove());
+    sketch.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    sketch.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-
+    let newSize = size * size;
 //Loop to create 16x16 grid of divs (256 squares total)
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement('div');
+    for (let i = 0; i < newSize; i++) {
+        const cell = document.createElement('div');
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
         //adds class name of "cell" to div
-        div.classList.add('cell');
+        cell.classList.add('cell');
         //adds div in html to become a child of container
-        sketch.insertAdjacentElement('beforeend', div);
+        sketch.insertAdjacentElement('beforeend', cell);
     }
 }
 sketch(16);
 
-//Create event listener for the hover effect
-    //Selects all of the classes that are called "cell" and store in variable div
-    const div = document.querySelectorAll('.cell');
-    //forEach goes through all of the variable's contents
-    div.forEach((div) => {
-        div.addEventListener('mouseover' , () => {
-        div.style.backgroundColor = 'black';
-        })
-    })
-
-
-//Create event listener to prompt user to input integer if they press the "change size" button
-function changeSize () {
-    const changeSize = document.querySelector('.button1');
-        changeSize.addEventListener('click', () => {
-        let size = prompt('Enter an integer for your desired grid-size (max = 100):');
-        // Checks if the user's input is an integer within scope
-        if (size >= 16 && size <= 100) {
-            container.textContent = "";
-                let newSize = size * size;
-                for (let i = 0; i < newSize; i++) {
-                    const div = document.createElement('div');
-                    //adds class name of "cell" to div
-                    div.classList.add('cell');
-                    const newContainer = document.querySelector('#container');
-                    //adds div in html to become a child of container
-                    container.appendChild(div);
-                }
-        }
-    }) 
-
- }
-
- changeSize();
-
-sketch();
+function changeSize(input) {
+    if(input >=2 && input <= 100) {
+        sketch(input);
+    } else {
+        console.log('Error - Please enter a value between 2 and 100');
+    }
+}
